4341cf864c39f0bb7ded68f6d66c33aec9ec6ac0
nbarbettini <nathanaelb@gmail.com>
2017-08-19 12:09:16 -0700

    Add view and models

---
+++ Models/TodoItem.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreTodo.Models
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+        
+        public bool IsDone { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public DateTimeOffset? DueAt { get; set; }
+    }
+}
---
+++ Models/TodoViewModel.cs
@@ -0,0 +1,7 @@
+namespace AspNetCoreTodo.Models
+{
+    public class TodoViewModel
+    {
+        public TodoItem[] Items { get; set; }
+    }
+}
