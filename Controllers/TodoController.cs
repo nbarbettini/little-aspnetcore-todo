1aa095049d4b6e5e40e6758ff0592b86c06f96d4
nbarbettini <nathanaelb@gmail.com>
2017-08-19 12:09:04 -0700

    Add controller

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Get to-do items from database
+
+            // Put items into a model
+
+            // Pass the view to a model and render
+        }
+    }
+}
